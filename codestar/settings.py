1c7abc57ffb3a7806a6032f36985c09976ab6fd8
JuanModzelewski <juanmodzelewski@gmail.com>
2024-09-09 14:59:32 +0200

    Created About app with content

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_summernote',
-    'blog'
+    'blog',
+    'about'
 ]
 
 MIDDLEWARE = [
